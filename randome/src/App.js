4092a542184ccbd1f3fb6c2d846a5e84542b9072
ghardonag <ghardonag@gmail.com>
2023-05-15 21:40:33 +0300

    Initial Randome app

--- randome/src/App.js
+++ randome/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import strings from "./strings.json";
 
 function App() {
+  const [randomString, setRandomString] = useState("");
+
+  const handleClick = () => {
+    const { strings: sourceStrings } = strings;
+    const randomIndex = Math.floor(Math.random() * sourceStrings.length);
+    const randomString = sourceStrings[randomIndex];
+    setRandomString(randomString);
+  };
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Random String App</h1>
+      <button onClick={handleClick}>Generate Random String</button>
+      <p>{randomString}</p>
     </div>
   );
 }
